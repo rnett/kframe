c8159bab4b096b6ff5f3dbe1a14f9f411fac3f1b
Ryan Nett <rnett@calpoly.edu>
2019-05-21 22:45:33 -0700

    spinner builders

--- src/main/kotlin/com.rnett.kframe/dom/bootstrap/components/Spinner.kt
+++ src/main/kotlin/com.rnett.kframe/dom/bootstrap/components/Spinner.kt
@@ -4,8 +4,13 @@ import com.rnett.kframe.dom.bootstrap.ContextType
 import com.rnett.kframe.dom.bootstrap.core.ClassElement
 import com.rnett.kframe.dom.bootstrap.utilities.srOnly
 import com.rnett.kframe.dom.span
+import com.rnett.kframe.structure.Builder
+import com.rnett.kframe.structure.DisplayHost
+import com.rnett.kframe.structure.KframeDSL
 import com.rnett.kframe.structure.TextElement
 import org.w3c.dom.HTMLDivElement
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
 
 class Spinner(color: ContextType) : ClassElement<HTMLDivElement, Spinner>("div", "spinner-border") {
     var color: ContextType by classes.classDelegate(color) { it.klass("text") }
@@ -27,4 +32,24 @@ class PulseSpinner(color: ContextType) : ClassElement<HTMLDivElement, PulseSpinn
     var color: ContextType by classes.classDelegate(color) { it.klass("text") }
 
     var small by classes.presentDelegate("sm").withClass
+}
+
+@KframeDSL
+inline fun DisplayHost.spinner(
+    color: ContextType,
+    klass: String = "", id: String = "",
+    builder: Builder<Spinner> = {}
+): Spinner {
+    contract { callsInPlace(builder, InvocationKind.EXACTLY_ONCE) }
+    return +Spinner(color)(klass, id, builder)
+}
+
+@KframeDSL
+inline fun DisplayHost.pulseSpinner(
+    color: ContextType,
+    klass: String = "", id: String = "",
+    builder: Builder<PulseSpinner> = {}
+): PulseSpinner {
+    contract { callsInPlace(builder, InvocationKind.EXACTLY_ONCE) }
+    return +PulseSpinner(color)(klass, id, builder)
 }
